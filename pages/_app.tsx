7b5a48601a0f0eb78e6927c0965fe2d45ff10149
Brice Amiel <briceamiel20@gmail.com>
2025-12-10 21:23:20 +0100

    fix: revert to cdn for leaflet css to restore map visibility

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'leaflet/dist/leaflet.css';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,9 @@ class MyDocument extends Document {
           {/* TailwindCSS CDN for styling */}
           <script src="https://cdn.tailwindcss.com"></script>
           {/* Leaflet CSS */}
-
+          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+            integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+            crossOrigin="" />
         </Head>
         <body>
           <Main />
